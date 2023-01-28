a474cf63960b069d2bc37cd2446c76ea16b5d2d0
lichterberci <lichterberci@gmail.com>
2023-01-28 17:41:54 +0100

    errors due to merge resolved

---
+++ pages/_app.tsx
@@ -0,0 +1,75 @@
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router';
+import Script from 'next/script'
+import { useEffect } from 'react';
+import HamburgerMenu from '../components/HamburgerMenu';
+import { GoogleLogPage } from '../scripts/GoogleAnalytics';
+import { Roboto } from "@next/font/google"
+
+const roboto = Roboto({
+    weight: '400',
+    subsets: ['latin'],
+});  
+
+export default function App({ Component, pageProps }: AppProps) {
+    
+    const router = useRouter();
+    
+    useEffect(() => {
+        const handleRouteChange = (url: string) => {
+            GoogleLogPage(url);
+        }
+
+        router.events.on("routeChangeStart", handleRouteChange);
+
+        return () => {
+            router.events.off('routeChangeStart', handleRouteChange)
+        }
+    }, [router.events])
+
+    const navBarItems = [
+		{
+			href: "/",
+			name: "Kezdőlap"
+		},
+		{
+			href: "/calculator",
+			name: "Ellenőrzés"
+		},
+		{
+			href: "/exercise",
+			name: "Gyakorlás"
+		},
+		{
+			href: "/about",
+			name: "Rólunk"
+		},
+		{
+			href: "/settings",
+			name: "Beállítások"
+		}
+	];
+    
+    return (<>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_KEY}`} />
+        
+        <Script strategy="lazyOnload" id="gtagScript">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.GTAG_KEY}', {
+            page_path: window.location.pathname,
+        });
+        `}
+        </Script>
+        
+        <main className={roboto.className}>
+            <HamburgerMenu items={navBarItems}/>
+
+            <Component {...pageProps} />
+        </main>
+    </>)
+        
+}
+    
--- pages/calculator.tsx
+++ pages/calculator.tsx
@@ -1,9 +1,5 @@
 import { MathJaxContext } from "better-react-mathjax";
-<<<<<<< HEAD
-import {useRef, useState} from "react";
-=======
 import {useEffect, useRef, useState} from "react";
->>>>>>> 0f875f0d246458565d80df36adacad801d821bf8
 import Image from "next/image"
 
 import { ISolutionData } from "../classes/ResponseData";
@@ -14,11 +10,9 @@ import styles from "../styles/calculator.module.css"
 import MathJaxConfig from "../mathjax.config.json"
 import LoadingAnim from "../public/LoadingAnim.gif"
 import Head from "next/head";
-<<<<<<< HEAD
-=======
+
 import { FirebaseInit } from "../scripts/Firebase";
 import { GetPreferences, SetCSSThemeFromLocalStorage } from "../scripts/Preferences";
->>>>>>> 0f875f0d246458565d80df36adacad801d821bf8
 
 let fetchAbortController = new AbortController();
 let fetchAbortSignal = fetchAbortController.signal;
@@ -30,15 +24,12 @@ export default function CalculatorPage (): JSX.Element {
     const [errorText, setErrorText] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-<<<<<<< HEAD
-=======
     useEffect (() => {
         FirebaseInit();
 
         SetCSSThemeFromLocalStorage();
     }, []);
 
->>>>>>> 0f875f0d246458565d80df36adacad801d821bf8
     const QueryDifferentiationAndUpdateUI = async () => {
 
         if (isLoading) {
@@ -77,55 +68,6 @@ export default function CalculatorPage (): JSX.Element {
         </Head>
         <main>
             <MathJaxContext version={3} config={MathJaxConfig}>
-<<<<<<< HEAD
-                <h1 className={styles.title}>
-                    <span className={styles.nemHiszed}>Nem hiszed?</span>
-                    <span>{" "}</span>
-                    <span className={styles.derivative}> Deriváld le!</span>
-                </h1>
-                <div className={styles.myContainer}>
-                    <span className={styles.inputHolder}>
-                        <input 
-                            className={styles.input}
-                            type="text" 
-                            placeholder="sin(x)" 
-                            onChange={e => inputRef.current = e.target.value}
-                            onKeyDown={e => {
-                                if (e.key == "Enter")
-                                    QueryDifferentiationAndUpdateUI()
-                            }}
-                        />
-
-                        <button className={styles.button} onClick={QueryDifferentiationAndUpdateUI}>
-                            Differentiate
-                        </button>
-                    </span>
-
-                    <div>
-                        {
-                            errorText != null
-                            && 
-                            <div className={styles.errorText}>
-                                { errorText }
-                            </div>
-                        }
-                    </div>
-
-                    <div className={styles.solutionWrapper}>
-                    {
-                        (() => {
-                        if (isLoading == false) {
-                            if (solutionData != null)
-                                return <Solution data={solutionData}/>
-                            else // error message is displayed, so we don't have to do anything here
-                                return <></>
-                        } 
-                        else { // display loading anim
-                            return <Image alt="Loading animation" src={LoadingAnim} width={100} height={100}/>
-                        }
-                        })()
-                    }
-=======
                 <div className={styles.myContainer}>
                     <div style={{marginTop:100}}>
                         <h1 className={styles.title}>
@@ -175,7 +117,6 @@ export default function CalculatorPage (): JSX.Element {
                             })()
                         }
                         </div>
->>>>>>> 0f875f0d246458565d80df36adacad801d821bf8
                     </div>
                 </div>
             </MathJaxContext>
