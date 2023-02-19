080cbdb70a73f78f58c893b9e6e3dc409c133403
lichterberci <lichterberci@gmail.com>
2023-02-19 22:42:37 +0100

    added roboto as link just to be sure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,12 @@ export default function Document() {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
 				<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin='true' />
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
 			</Head>
 			<body style={{width: "100%", height: "100%", margin: 0}}>
 				{/* <Navbar items={navBarItems} /> */}
-				
 				<Main />
 				<NextScript />
 			</body>
