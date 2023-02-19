840adc209fce34a72743b48c9250ea3d2f853059
lichterberci <lichterberci@gmail.com>
2023-02-19 22:45:15 +0100

    minor adjustments

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default function Document() {
 				<link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin='true' />
 				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
 			</Head>
-			<body style={{width: "100%", height: "100%", margin: 0}}>
+			<body style={{width: "100%", height: "100%", margin: 0, fontFamily: "'Roboto', sans-serif"}}>
 				{/* <Navbar items={navBarItems} /> */}
 				<Main />
 				<NextScript />
