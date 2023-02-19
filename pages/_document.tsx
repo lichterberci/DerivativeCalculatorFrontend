9d3084fd2f48b22fd021b1741d9ad4eb0acf83a2
lichterberci <lichterberci@gmail.com>
2023-02-19 22:36:28 +0100

    added source code pro to header as link to load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang="hu" style={{ backgroundColor: "var(--background-color)", color: "var(--text-color"}}>
-			<Head />
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+				<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap" rel="stylesheet" />
+			</Head>
 			<body style={{width: "100%", height: "100%", margin: 0}}>
 				{/* <Navbar items={navBarItems} /> */}
 				
